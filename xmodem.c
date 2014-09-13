ad4f5c87ad7f34d48c3ac5040cec6f3ab6a456de
Matthias <tqzamf@gmail.com>
2014-09-13 16:42:10 +0200

    receive-only xmodem block driver (~400 bytes)

---
+++ xmodem.c
@@ -0,0 +1,79 @@
+#include <stdint.h>
+#include "hw.h"
+
+#define XMODEM_TIMEOUT TIMER_TIMEOUT(10000)
+#define ACK 0x06
+#define NAK 0x15
+#define EOT 0x04
+#define STX 0x02
+#define SOH 0x01
+#define CRC 'C'
+
+static uint8_t xmodem_ack, xmodem_block;
+
+// ~32 bytes
+export void xmodem_init(void) {
+	xmodem_ack = NAK;
+	xmodem_block = 1;
+}
+
+// ~380 bytes
+export uint32_t xmodem_get_block(uint8_t *base) {
+	UART_TX(xmodem_ack);
+	xmodem_ack = NAK;
+	register uint32_t timeout_start = TIMER_START();
+	
+	// OUCH. but it does get rid of the "uninitialized" warning.
+	uint8_t block = block, invblock = invblock;
+	uint8_t checksum = 0;
+	for (uint8_t pos = 0; pos <= 128 + 3; pos++) {
+		uint8_t ch = UART_RX_TIMEOUT(timeout_start, XMODEM_TIMEOUT);
+		
+		switch (pos) {
+		case 0:
+			//header = ch;
+			if (ch == EOT)
+				// there'll be no more blocks; signal EOF
+				return 0;
+			if (ch != SOH)
+				// wrong header, or 1k blocks
+				return -1;
+			break;
+		case 1:
+			block = ch;
+			break;
+		case 2:
+			invblock = ch;
+			break;
+		default:
+			base[pos - 3] = ch;
+			checksum += ch;
+			break;
+		case 128 + 3:
+			checksum -= ch;
+			break;
+		}
+	}
+
+	if (block != ~invblock)
+		// block number corrupted
+		return -1;
+	if (block == xmodem_block) {
+		// previous block was re-sent. acknowledge to client, but we'll
+		// need to retry for the current block.
+		xmodem_ack = ACK;
+		return -1;
+	}
+	if (block != xmodem_block + 1)
+		// wrong block number
+		return -1;
+
+	if (checksum != 0)
+		// incorrect checksum. retry.
+		return -1;
+	
+	// correct block received; next one please
+	xmodem_block = block;
+	xmodem_ack = ACK;
+	return 0;
+}
