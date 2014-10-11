7f9f4674b9e5c275ab1bdb9b10a8bfadd7b1f332
Matthias <tqzamf@gmail.com>
2014-10-11 17:54:37 +0200

    optimize handling of long xmodem timeout
    we don't have to initialize that anywhere except after receiving a valid
    block, because it can never be checked before receiving a valid block: it
    is only ever checked after receiving the header, and the header must be a
    valid bock...

--- xmodem.c
+++ xmodem.c
@@ -16,14 +16,16 @@ static uint32_t xmodem_timeout;
 export void xmodem_init(void) {
 	xmodem_ack = NAK;
 	xmodem_block = 0;
+	// no need to initialize xmodem_timeout here. it's ignored while in
+	// the header, and it takes a valid block to get out of the header.
+	// xmodem_timeout IS initialized (reset actually) when a valid block
+	// is received.
 }
 
 export int16_t xmodem_get_block(uint8_t *base, int in_header) {
 	// if there was no useable block for 10s, give up and try again.
 	// don't do that while waiting for the header, though.
-	if (in_header)
-		xmodem_timeout = TIMER_START();
-	if (TIMER_TIMED_OUT(xmodem_timeout, XMODEM_LONG_TIMEOUT))
+	if (!in_header && TIMER_TIMED_OUT(xmodem_timeout, XMODEM_LONG_TIMEOUT))
 		return -2;
 
 	// no need to flush here. the RX will wait until the sender is
