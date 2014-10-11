24443fe69545da85da9703dbbb8b860d6523ca45
Matthias <tqzamf@gmail.com>
2014-10-11 21:21:19 +0200

    implement error correction for ECC
    also change ecctester to test for proper single-bit error correction and
    double-bit error detection.

--- ecc.c
+++ ecc.c
@@ -1,21 +1,18 @@
 #include <stdint.h>
 
 static inline uint32_t ecc_correct(uint8_t *data, uint32_t ecc) {
-	// repack ECC bits into a standard odd-parity hamming code and a
-	// global parity bit stored with an 11-way repetition code. this
-	// makes all further calculations a lot easier. it also shows that
-	// standard NAND ECC is actually a rather dumb way of storing a
-	// normal SECDED extended hamming code...
-	uint32_t hamming_read = 0, parity_read = 0;
+	// repack ECC bits into a pair of standard hamming codes, with even
+	// and odd parity respectively. this makes all further calculations
+	// a bit easier.
+	uint32_t hamming1_read = 0, hamming2_read = 0;
 	for (uint32_t bit = 1; bit & 0x7ff; bit <<= 1) {
-		parity_read |= ecc & bit;
+		hamming1_read |= ecc & bit;
 		ecc >>= 1;
-		hamming_read |= ecc & bit;
+		hamming2_read |= ecc & bit;
 	}
-	parity_read ^= hamming_read;
 	
 	// recalculate parities from the data actually read off the device.
-	uint32_t rpar = 0xff, cpar = 0x07;
+	uint32_t parity = 0x7ff;
 	for (uint32_t pos = 0; pos < 256; pos++) {
 		uint8_t byte = data[pos];
 		
@@ -24,12 +21,12 @@ static inline uint32_t ecc_correct(uint8_t *data, uint32_t ecc) {
 		// belongs in the final result to save a shift.
 		uint32_t l2 = byte ^ (byte << 2);
 		uint32_t l2r1 = l2 ^ (l2 >> 1);
-		cpar ^= l2r1 & 4;
+		parity ^= l2r1 & 4;
 		uint32_t r4 = byte ^ (byte >> 4);
 		uint32_t r4l1 = r4 ^ (r4 << 1);
-		cpar ^= r4l1 & 2;
+		parity ^= r4l1 & 2;
 		uint32_t r4r2 = r4 ^ (r4 >> 2);
-		cpar ^= r4r2 & 1;
+		parity ^= r4r2 & 1;
 		
 		// calculate overall parity of the byte. the result ends up in
 		// the lowest bit; we then use negation to mirror that bit into
@@ -37,27 +34,59 @@ static inline uint32_t ecc_correct(uint8_t *data, uint32_t ecc) {
 		uint32_t r4r2r1 = r4r2 ^ (r4r2 >> 1);
 		uint32_t par = -(r4r2r1 & 1);
 		
-		// calculate row parity. the neat thing about the normal, packed
-		// representation is that the parity of a byte should contribute
-		// to exactly those bits of the final result which are set in
-		// the position of the byte, ie. row parity can be calculated in
-		// parallel for all bits.
-		rpar ^= par & pos;
 		// the column parity code above actually calculates the parity
 		// over the wrong bits. we correct that here, and in doing so
-		// calculate the overall parity as well. this is slightly smaller
-		// than calculating the parity over the right bits, because then
-		// we'd have to calculate the overall parity separately.
-		cpar ^= par;
+		// calculate both the overall parity and the row parity as well.
+		// this is smaller than calculating the parity over the right
+		// bits in the first place, because then we'd have to calculate
+		// the other parities separately.
+		// for row parity, we use the neat fact the in normal, packed
+		// representation, the parity of a byte should contribute to
+		// exactly those bits of the final result which are set in the
+		// position of the byte, ie. row parity can be calculated in
+		// parallel for all bits.
+		// all in all, this means we have to XOR the byte's parity into:
+		// - the column parity bits so they cover the right bits
+		// - any row parity bits they should affect
+		// - some higher-order bits, for overall parity
+		uint32_t affected = 0x3ff807 | (pos << 3);
+		parity ^= par & affected;
 	}
-	uint32_t hamming_calc = (rpar << 3) | (cpar & 0x7);
-	uint32_t parity_calc = (cpar >> 3) & 0x7ff;
+	uint32_t hamming2_calc = parity & 0x7ff;
+	// in the code above, we only calculate the parity for the odd parity
+	// hamming code. we can now easily derive the even parity ones by
+	// XORing them with the overall parity.
+	uint32_t hamming1_calc = hamming2_calc ^ (parity >> 11);
+	// calculate syndrome for both hamming codes.
+	uint32_t syndrome1 = hamming1_calc ^ hamming1_read;
+	uint32_t syndrome2 = hamming2_calc ^ hamming2_read;
 	
-	if (hamming_calc == hamming_read && parity_read == parity_calc)
-		// all bits except the unused ones agree, ie. there was no error.
+	if (syndrome1 == 0 && syndrome2 == 0)
+		// all bits agree with the calculated ones. no error.
 		return 0;
 	
-	// TODO detect and correct single-bit errors
+	if ((syndrome1 ^ syndrome2) == 0x7ff) {
+		// overall parity is self-consistent, but disagrees with what we
+		// have calculated. this means there must have been a correctable
+		// single-bit error (or a triple bit error that we are about to
+		// mis-correct, but that's permissible).
+		uint32_t errpos_bit = 1 << (syndrome2 & 0x7);
+		uint32_t errpos_byte = syndrome2 >> 3;
+		data[errpos_byte] ^= errpos_bit;
+		return 1;
+	}
+	
+	uint32_t bitcount = (syndrome1 << 11) | syndrome2;
+	if ((bitcount & (bitcount - 1)) == 0)
+		// overall parity is correct, except for a single bit. this means
+		// there was an error in the ECC bits. an error in the ECC data
+		// does not need correcting, but still has to be reported because
+		// another error would make the block uncorrectable.
+		// the single-bit-check is a variation of the classic Brian
+		// Kernighan way of bitcounting. we actually use it to clear the
+		// lowest bit, wherever it may be. if any other bit was set too,
+		// then the result is necessarily nonzero.
+		return 1;
 	
 	// uncorrectable error
 	return 2;
--- ecctester.c
+++ ecctester.c
@@ -2,24 +2,52 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/mman.h>
+#include <time.h>
 #include <err.h>
-#include <setjmp.h>
-#include <signal.h>
+#include <stdlib.h>
 
 #include "ecc.c"
 
+static inline int affects(int errpos_byte, uint8_t errpos_bit) {
+	int num_errors_lower, num_errors_upper;
+	if (errpos_byte < 256) {
+		num_errors_lower = 1;
+		num_errors_upper = 0;
+	} else if (errpos_byte < 512) {
+		num_errors_lower = 0;
+		num_errors_upper = 1;
+	} else if (errpos_byte == 512 + 0 || errpos_byte == 512 + 1) {
+		num_errors_lower = 1;
+		num_errors_upper = 0;
+	} else if (errpos_byte == 512 + 3 || errpos_byte == 512 + 6) {
+		num_errors_lower = 0;
+		num_errors_upper = 1;
+	} else if (errpos_byte == 512 + 2 && errpos_bit != 0x01 && errpos_bit != 0x02) {
+		num_errors_lower = 1;
+		num_errors_upper = 0;
+	} else if (errpos_byte == 512 + 7 && errpos_bit != 0x01 && errpos_bit != 0x02) {
+		num_errors_lower = 0;
+		num_errors_upper = 1;
+	} else {
+		// OOB bit not used by ECC, so no error expected
+		num_errors_lower = 0;
+		num_errors_upper = 0;
+	}
+	return num_errors_lower + (num_errors_upper << 8);
+}
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
-		fprintf(stderr, "usage: ecctester path-to-nand-image >output\n");
+		fprintf(stderr, "usage: ecctester path-to-nand-image\n");
 		return -1;
 	}
 	
+	srand(time(NULL));
 	int fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 		err(1, "cannot open %s", argv[1]);
 
-	uint32_t buffer[512/4 + 16/4];
+	uint8_t buffer[512 + 16];
 	int page = 0;
 	while (1) {
 		int res = read(fd, buffer, 512 + 16);
@@ -28,26 +56,100 @@ int main(int argc, char **argv) {
 		if (res != 512 + 16)
 			err(1, "cannot read page %04x + OOB", page);
 		
-		uint8_t *oob = ((uint8_t *) buffer) + 512;
-		
+		uint8_t *oob = buffer + 512;
 		uint8_t ecc0, ecc1, ecc2;
+		
+		// test ECC calculation. the input is correct so there should be
+		// no disagreement with calculated values.
+		
 		ecc0 = oob[0];
 		ecc1 = oob[1];
 		ecc2 = oob[2];
 		uint32_t ecc = (ecc0 << 14) | (ecc1 << 6) | (ecc2 >> 2);
-		uint32_t num_errors = ecc_correct(((uint8_t *) buffer) + 0, ecc);
+		uint32_t num_errors = ecc_correct(buffer + 0, ecc);
 		if (num_errors)
-			fprintf(stderr, "page %04xL has %d errors\n", page, num_errors);
+			fprintf(stderr, "error: page %04xL has %d errors\n", page, num_errors);
 
 		ecc0 = oob[3];
 		ecc1 = oob[6];
 		ecc2 = oob[7];
 		ecc = (ecc0 << 14) | (ecc1 << 6) | (ecc2 >> 2);
-		num_errors = ecc_correct(((uint8_t *) buffer) + 256, ecc);
+		num_errors = ecc_correct(buffer + 256, ecc);
 		if (num_errors)
-			fprintf(stderr, "page %04xU has %d errors\n", page, num_errors);
+			fprintf(stderr, "error: page %04xU has %d errors\n", page, num_errors);
 		
-		write(1, buffer, 512);
+		// test single-bit error correction. corrupt a bit and see if it gets
+		// corrected properly.
+		
+		uint8_t errpos_bit = 1 << (page & 0x7);
+		int errpos_byte = (page >> 3) % (512 + 16);
+		fprintf(stderr, "page %04x: corrupting %03x bit %02x\n",
+				page, errpos_byte, errpos_bit);
+		int expect = affects(errpos_byte, errpos_bit);
+		int num_errors_lower = expect & 0xff;
+		int num_errors_upper = expect >> 8;
+		uint8_t orig_byte = buffer[errpos_byte];
+		buffer[errpos_byte] = orig_byte ^ errpos_bit;
+		
+		ecc0 = oob[0];
+		ecc1 = oob[1];
+		ecc2 = oob[2];
+		ecc = (ecc0 << 14) | (ecc1 << 6) | (ecc2 >> 2);
+		num_errors = ecc_correct(buffer + 0, ecc);
+		if (num_errors != num_errors_lower)
+			fprintf(stderr, "error: page %04xL has %d errors\n", page, num_errors);
+
+		ecc0 = oob[3];
+		ecc1 = oob[6];
+		ecc2 = oob[7];
+		ecc = (ecc0 << 14) | (ecc1 << 6) | (ecc2 >> 2);
+		num_errors = ecc_correct(buffer + 256, ecc);
+		if (num_errors != num_errors_upper)
+			fprintf(stderr, "error: page %04xU has %d errors\n", page, num_errors);
+		
+		if (orig_byte != buffer[errpos_byte] && errpos_byte < 512)
+			fprintf(stderr, "error: page %04x error in byte %03x bit %02x not corrected:"
+					"%02x != %02x\n", page, errpos_byte, errpos_bit, orig_byte, buffer[errpos_byte]);
+		buffer[errpos_byte] = orig_byte;
+		
+		// test double-bit error detection. corrupt two bits and see if it gets
+		// detected properly.
+		
+		// random second bit. not exhaustive, but good enough.
+		int random = rand();
+		uint8_t errpos2_bit = 1 << (random & 0x7);
+		int errpos2_byte = (random >> 3) % (512 + 16);
+		fprintf(stderr, "page %04x: corrupting %03x bit %02x and %03x bit %02x\n",
+				page, errpos_byte, errpos_bit, errpos2_byte, errpos2_bit);
+		expect = affects(errpos_byte, errpos_bit);
+		num_errors_lower = expect & 0xff;
+		num_errors_upper = expect >> 8;
+		expect = affects(errpos2_byte, errpos2_bit);
+		num_errors_lower += expect & 0xff;
+		num_errors_upper += expect >> 8;
+		if (errpos2_bit == errpos_bit && errpos2_byte == errpos_byte)
+			// flipping the same bit back and forth. not an effective test,
+			// but don't misreport it as an error.
+			num_errors_lower = num_errors_upper = 0;
+		buffer[errpos_byte] = buffer[errpos_byte] ^ errpos_bit;
+		buffer[errpos2_byte] = buffer[errpos2_byte] ^ errpos2_bit;
+		
+		ecc0 = oob[0];
+		ecc1 = oob[1];
+		ecc2 = oob[2];
+		ecc = (ecc0 << 14) | (ecc1 << 6) | (ecc2 >> 2);
+		num_errors = ecc_correct(buffer + 0, ecc);
+		if (num_errors != num_errors_lower)
+			fprintf(stderr, "error: page %04xL has %d errors\n", page, num_errors);
+
+		ecc0 = oob[3];
+		ecc1 = oob[6];
+		ecc2 = oob[7];
+		ecc = (ecc0 << 14) | (ecc1 << 6) | (ecc2 >> 2);
+		num_errors = ecc_correct(buffer + 256, ecc);
+		if (num_errors != num_errors_upper)
+			fprintf(stderr, "error: page %04xU has %d errors\n", page, num_errors);
+		
 		page++;
 	}
 
