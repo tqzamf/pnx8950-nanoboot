cfbf0633330955cc48c6aaa17f6e4c928d7aac1b
Matthias <tqzamf@gmail.com>
2014-10-12 18:08:18 +0200

    use longer delay for bus pirate

--- bpsend.pl
+++ bpsend.pl
@@ -1,5 +1,4 @@
-#!/usr/bin/perl -wp
-use strict;
+#!/usr/bin/perl -p
 use Time::HiRes qw(usleep);
 
 ## usage:
@@ -10,6 +9,7 @@ use Time::HiRes qw(usleep);
 # implicit main loop. noting to do except delay the lines we're sending
 # so we don't overwhelm the bus pirate. this way the reprogramming is
 # slower, but at least it works.
-# a 10ms delay is enough because the EEPROM needs at most 5ms to program
-# a page.
-usleep 10000;
+# a 50ms delay seems to be enough for the bus pirate to keep up with our
+# commands.
+usleep 50000;
+print STDERR "\r" . $n++;
