bb14bff5e40124d1dc0d18dd2102e8ec4daa6eb1
Matthias <tqzamf@gmail.com>
2014-10-12 18:06:54 +0200

    fix bus pirate EEPROM loading
    needs to send a stop bit after each page. a repeated start bit aborts the
    write. also, the second virtual chip starts with address 0xa2, not 0xa1,
    because of the read/write bit.

--- eecompile/src/BusPirateFormatter.java
+++ eecompile/src/BusPirateFormatter.java
@@ -17,14 +17,15 @@ public class BusPirateFormatter extends Formatter {
 			// next address header. generate a new start condition and send the
 			// next address.
 			if (i % 16 == 0)
-				out.printf("[0xa%1x 0x%02x", i >> 8, i & 255);
+				out.printf("[0xa%1x 0x%02x", i >> 8 << 1, i & 255);
 			out.printf(" 0x%02x", b & 255);
-			// no need for a stop condition until the very end. we can just keep
-			// doing re-start conditions until we're done.
+			// send a stop bit so that the chip starts writing. a re-start
+			// aborts the write.
 			if (i++ % 16 == 15)
-				out.println();
+				out.printf("]\n");
 		}
-		out.printf("]\n");
+		if (i % 16 != 0)
+			out.printf("]\n");
 		out.close();
 	}
 }
