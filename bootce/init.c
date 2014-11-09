d1aca32008ed22b86f695d1c413b701c14d608c4
Matthias <tqzamf@gmail.com>
2014-11-09 16:38:05 +0100

    compiler trickery to reduce size of peripheral init

--- bootce/init.c
+++ bootce/init.c
@@ -4,11 +4,32 @@
 #define RESET (*((uint32_t *) 0xbbe60000))
 #define ASSERT_SYS_RESET  1
 #define RELEASE_SYS_RESET 2
-#define CLOCK(x, y) *((uint32_t *) 0xbbe47##x) = 0x##y;
-#define GLOBAL1(x, y) *((uint32_t *) 0xbbe63##x) = 0x##y;
-#define GLOBAL2(x, y) *((uint32_t *) 0xbbe4d##x) = 0x##y;
-#define UART1(x, y) *((uint32_t *) 0xbbe4a##x) = 0x##y;
-#define UART2(x, y) *((uint32_t *) 0xbbe4b##x) = 0x##y;
+
+#define DECLARE(name) \
+	volatile uint32_t *name##_LOWER = (void *) name##_BASE; \
+	volatile uint32_t *name##_UPPER = (void *) (name##_BASE + 0x10000); \
+	asm("" : "=r" (name##_LOWER) : "r" (name##_LOWER)); \
+	asm("" : "=r" (name##_UPPER) : "r" (name##_UPPER));
+#define SET(name, mod, off, val) \
+	do { \
+		if (0x##mod >= 0x8) \
+			name##_UPPER[(0x##mod##off - 0x10000) >> 2] = 0x##val; \
+		else \
+			name##_LOWER[0x##mod##off >> 2] = 0x##val; \
+	} while (0)
+
+#define BBE4_BASE 0xbbe40000
+#define DECLARE_CLOCK DECLARE(BBE4)
+#define DECLARE_UART1
+#define DECLARE_UART2
+#define DECLARE_GLOBAL2
+#define BBE6_BASE 0xbbe60000
+#define DECLARE_GLOBAL1 DECLARE(BBE6)
+#define CLOCK(x, y) SET(BBE4, 7, x, y)
+#define GLOBAL1(x, y) SET(BBE6, 3, x, y)
+#define GLOBAL2(x, y) SET(BBE4, d, x, y)
+#define UART1(x, y) SET(BBE4, a, x, y)
+#define UART2(x, y) SET(BBE4, b, x, y)
 
 extern void _start(void);
 extern void __image_end(void);
@@ -24,16 +45,15 @@ static inline void init(void) {
 	// WinCE-shaped hole so we don't overwrite ourselves, and don't kill
 	// our stack either.
 	#define INIT(from, to) \
-		for (uint32_t addr = (from); addr != (to); addr += 16) { \
+		for (uint32_t addr = (from); addr != (to); addr += 4) { \
 			uint32_t *word = (uint32_t *) addr; \
 			word[0] = 0; \
-			word[1] = 0; \
-			word[2] = 0; \
-			word[3] = 0; \
 		}
 	INIT(0x80000000, ((uint32_t) _start) - STACK_SIZE);
 	INIT((uint32_t) __image_end, 0x88000000);
-	cache_flush();
+	// not flushing cache here; it's pointless. data accesses to the
+	// cleared regions go through cache, which is self-consistent. and
+	// executing from the cleared regions is probably incorrect anyway.
 
 	// CMEM registers, as set up by FlashReader. U-Boot has a different
 	// layout.
@@ -60,6 +80,7 @@ static inline void init(void) {
 	// perform some extra hardware init that the original bootscript does
 	// but nanoboot omits.
 	// start some extra (AV system) clocks
+	DECLARE_CLOCK;
 	CLOCK(004, 00b40904); // TM32 1
 	CLOCK(008, 3828050c); // QVCP 1
 	CLOCK(00c, 3828050c); // QVCP 2
@@ -76,6 +97,7 @@ static inline void init(void) {
 	CLOCK(504, 00000073); // MBS 3
 	CLOCK(708, 00000003); // unknown
 	// some UART setup normally done by FlashReader
+	DECLARE_UART2;
 	UART2(000, 01060000); // flush buffers and configure for 8-bit
 	UART2(fe8, 00001fff); // clear any pending interrupts
 	UART2(004, 00000003); // setup modem & flow control lines
@@ -84,15 +106,18 @@ static inline void init(void) {
 	// leave the baud rate as it is, even if it's wrong. UART1 is used
 	// for the kernel log only, so it's actually convenient if the baud
 	// rate doesn't have to be changed on the other end.
+	DECLARE_UART1;
 	UART1(000, 01060000); // flush / 8-bit
 	UART1(fe8, 00001fff); // interrupts
-	// misc registers (for good measure; not really required)
-	GLOBAL2(60c, 00000001); // probably VIP control
-	GLOBAL2(050, 00000001); // PCI INTA enable
 	// pretend we're the original bootscript.
+	DECLARE_GLOBAL1;
 	GLOBAL1(500, 00130001);
 	GLOBAL1(504, 00000006);
 	GLOBAL1(508, 00010014);
+	// misc registers (for good measure; not really required)
+	DECLARE_GLOBAL2;
+	GLOBAL2(60c, 00000001); // probably VIP control
+	GLOBAL2(050, 00000001); // PCI INTA enable
 	// manufacturer ID, hardware ID and board UUID, as set by the
 	// original bootschript
 	GLOBAL2(500, ffffffff);
